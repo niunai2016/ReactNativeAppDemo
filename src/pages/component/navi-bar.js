b8bf2eb3e1beb132a2807d813b82e525c40e0d4e
cangnaiwen <cangnaiwen@digitalfin.cn>
2019-04-15 14:16:47 +0800

    update

---
+++ src/pages/component/navi-bar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class NaviBar extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>顶部导航栏</Text>
+      </View>
+    )
+  }
+}
