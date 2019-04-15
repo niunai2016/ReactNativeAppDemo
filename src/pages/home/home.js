5819e68ef018ba791567b2922f3e0a60e892d2a3
cangnaiwen <cangnaiwen@digitalfin.cn>
2019-04-15 10:59:23 +0800

    home加入list跳转

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import {colors} from '../assets/styles/colors-theme';
+import history from '../common/history';
 
 export default class Home extends Component {
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Home</Text>
+        <TouchableOpacity style={styles.button} onPress={() => history.push(this, '/list', {name: 'cangnaiwen'})}>
+          <Text style={styles.buttonText}>跳转到List</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.flatten({
+  button: {
+    marginTop: 20,
+    width: 100,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: colors.statusBarColor
+  },
+  buttonText: {
+    color: '#fff'
+  }
+})
