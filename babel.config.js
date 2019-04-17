fcfd76bf167f55bd456582b8c50f09f6d894b8c2
cangnaiwen <cangnaiwen@digitalfin.cn>
2019-04-17 10:51:56 +0800

    引入antd-mobile-rn

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'react-native'
+  ],
+  plugins: [
+    [
+      'import',
+      {
+        'libraryName': 'antd-mobile-rn'
+      }
+    ]
+  ]
 };
