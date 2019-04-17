9750116a038e6f9b71307cf4687d6794603bc447
cangnaiwen <cangnaiwen@digitalfin.cn>
2019-04-17 11:23:52 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    'react-native'
-  ],
-  plugins: [
-    [
-      'import',
-      {
-        'libraryName': 'antd-mobile-rn'
-      }
-    ]
-  ]
+  presets: ['module:metro-react-native-babel-preset'],
 };
