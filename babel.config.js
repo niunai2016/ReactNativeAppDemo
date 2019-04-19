939bb794e582c39ebd50e8f5063eac4305bf24d3
cangnaiwen <cangnaiwen@digitalfin.cn>
2019-04-19 10:40:42 +0800

    add babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', 'react-native'],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
   plugins: [
+    '@babel/transform-flow-strip-types',
+    [
+      '@babel/plugin-proposal-decorators', { 'legacy' : true }
+    ],
+    [
+      '@babel/plugin-proposal-class-properties', {'loose': true}
+    ],
+    [
+      '@babel/plugin-transform-runtime', {}
+    ],
     ['import', { 'libraryName': '@ant-design/react-native' }]
   ]
 };
